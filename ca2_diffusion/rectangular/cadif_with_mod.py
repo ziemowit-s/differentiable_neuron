6dd6f8d5241f5ac84bf6c8996ed195b9d1bef6ae
Ziemowit S <ziemowit.slawinski@gmail.com>
2019-11-29 16:40:04 +0100

    removed other compartments from ca2+ mod-based diffusion in spine head; changed L=diam=1

--- ca2_diffusion/rectangular/cadif_with_mod.py
+++ ca2_diffusion/rectangular/cadif_with_mod.py
@@ -10,33 +10,11 @@ from ca2_diffusion.rectangular.utils import record, get_3d_concentration
 
 class Cell:
     def __init__(self, name):
-        """
-        spine_head->spine_neck->dendrite
-        """
         self._name = name
-
         self.head = h.Section(name='head', cell=self)
-        self.head.L = 0.5
-        self.head.diam = 0.5
+        self.head.L = 1
+        self.head.diam = 1
         self.head.nseg = 11
-
-        self.neck = h.Section(name='neck', cell=self)
-        self.neck.L = 0.5
-        self.neck.diam = 0.1
-        self.neck.nseg = 11
-
-        self.dend = h.Section(name='dend', cell=self)
-        self.dend.L = 11
-        self.dend.diam = 1.0
-        self.dend.nseg = 110
-
-        self.head.insert('cadifusrect')
-        self.neck.insert('cadifusrect')
-
-        self.all = [self.dend, self.neck, self.head]
-
-        self.head.connect(self.neck)
-        self.neck.connect(self.dend(0.5))
 
     def __repr__(self):
         return "Cell[{}]".format(self._name)
