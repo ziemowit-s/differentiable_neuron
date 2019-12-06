2b8492af2dee42c7588e5d3be0d1d0adea98b8d9
Ziemowit Slawinski <ziemowit.slawinski@gmail.com>
2019-12-06 10:56:41 +0100

    minor changes

--- spine_location/graham2014.py
+++ spine_location/graham2014.py
@@ -3,6 +3,9 @@ from random import randint
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Function adapted from Graham et al. 2014
+# it is just random uniform distribution over all dendritic tree
+
 SYN_NUM = 500
 compartments = np.random.randint(50, 300, 200)
 lmax = np.sum(compartments)
@@ -12,13 +15,13 @@ xs = []
 for i in range(SYN_NUM):
     l = 0
     r = randint(0, lmax)
-    for c in compartments:
+    for index, c in enumerate(compartments):
         l += c
         y = 0
         if l > r:
             y = (r - l + c)/c
             ys.append(y)
-            xs.append(c)
+            xs.append(index)
             break
 
 plt.scatter(xs, ys, alpha=0.5)
